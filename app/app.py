30f5c80cbb79af3f59b2a290082b9afeff73a3cf
agemcipe <jonathan.haas.ger@gmail.com>
2021-03-14 23:07:43 +0000

    Add option to choose distance metric

--- app/app.py
+++ app/app.py
@@ -1,81 +1,96 @@
 import os
 import cv2
 import numpy as np
- 
+
 
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from werkzeug.utils import secure_filename
- 
+
 
 from pyimagesearch.colordescriptor import ColorDescriptor
-from pyimagesearch.searcher import Searcher
+from pyimagesearch.searcher import Searcher, IMPLEMENTED_METRICS
 
 
 # create flask instance
 app = Flask(__name__)
 
-INDEX = os.path.join(os.path.dirname(__file__), 'index.csv')
+INDEX = os.path.join(os.path.dirname(__file__), "index.csv")
 
 # main route
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', preview="static/init-preview.png")
+    return render_template(
+        "index.html", preview="static/init-preview.png", metrics=IMPLEMENTED_METRICS
+    )
+
 
 # image database url list route
-@app.route('/list', methods=['POST'])
+@app.route("/list", methods=["POST"])
 def image_list():
 
     if request.method == "POST":
 
         try:
 
-            imgList = [img for img in list(os.listdir(os.path.join(os.path.dirname(__file__), 'static/images/'))) if img[-4:] in ('.png', '.jpg', '.gif')]
+            imgList = [
+                img
+                for img in list(
+                    os.listdir(
+                        os.path.join(os.path.dirname(__file__), "static/images/")
+                    )
+                )
+                if img[-4:] in (".png", ".jpg", ".gif")
+            ]
 
             return jsonify(imgList=imgList)
-        
+
         except Exception as e:
             return jsonify({"sorry": "Sorry, no results! Please try again."}), 500
 
 
 # search route
-@app.route('/search', methods=['POST'])
+@app.route("/search", methods=["POST"])
 def search():
- 
+
     if request.method == "POST":
 
         RESULTS_ARRAY = []
 
         # get url
-        image_url = request.form.get('img')
- 
+        image_url = request.form.get("img")
+
         try:
- 
+
             # initialize the image descriptor
             cd = ColorDescriptor((8, 12, 3))
- 
+
             # load the query image and describe it
             from skimage import io
             import cv2
 
-            query = cv2.imread(os.path.join(os.path.dirname(__file__), 'static/images/'+image_url))
+            query = cv2.imread(
+                os.path.join(os.path.dirname(__file__), "static/images/" + image_url)
+            )
             features = cd.describe(query)
- 
+
             # perform the search
             searcher = Searcher(INDEX)
-            results = searcher.search(features)
- 
+
+            metric = request.form.get("metric")
+            results = searcher.search(features, metric=metric)
+
             # loop over the results, displaying the score and image name
             for (score, resultID) in results:
-                RESULTS_ARRAY.append(
-                    {"image": str(resultID), "score": str(score)})
+                RESULTS_ARRAY.append({"image": str(resultID), "score": str(score)})
             # return success
-            return jsonify(results=(RESULTS_ARRAY[:101]), preview="images/"+image_url)
- 
+            return jsonify(results=(RESULTS_ARRAY[:101]), preview="images/" + image_url)
+
         except Exception as e:
             print(str(e))
             # return error
             return jsonify({"sorry": "Sorry, no results! Please try again."}), 500
 
+
 # run!
-if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
--- app/pyimagesearch/searcher.py
+++ app/pyimagesearch/searcher.py
@@ -1,46 +1,60 @@
 # import the necessary packages
 import numpy as np
 import csv
+import scipy.spatial.distance
+
+IMPLEMENTED_METRICS = ["chi2", "canberra"]
+
 
 class Searcher:
-	def __init__(self, indexPath):
-		#store our index path
-		self.indexPath = indexPath
+    def __init__(self, indexPath):
+        # store our index path
+        self.indexPath = indexPath
 
-	def search(self, queryFeatures, limit=101):
-		#initialize dictionary of results
-		results = {}
+    def search(self, queryFeatures, limit=101, metric="chi2"):
 
-		#open the index file
-		with open(self.indexPath) as f:
-			#initialize the CSV reader
-			reader = csv.reader(f)
+        if metric not in IMPLEMENTED_METRICS:
+            raise ValueError(f"Unknown metric '{metric}'")
 
-			#loop over the rows in the index
-			for row in reader:
-				#parse out the image ID and features, then compute the
-				#chi-squared dist. b/w the features in our index
-				#and our query features
-				features = [float(x) for x in row[1:]]
-				d = self.chi2_distance(features, queryFeatures)
+        # initialize dictionary of results
+        results = {}
 
-				#Update dictionsary
-				#key is image id, value is similarity
-				results[row[0]] = d
+        # open the index file
+        with open(self.indexPath) as f:
+            # initialize the CSV reader
+            reader = csv.reader(f)
 
-			#close reader
-			f.close()
+            # loop over the rows in the index
+            for row in reader:
+                # parse out the image ID and features, then compute the
+                # chi-squared dist. b/w the features in our index
+                # and our query features
+                features = [float(x) for x in row[1:]]
+                if metric == "chi2":
+                    d = self.chi2_distance(features, queryFeatures)
+                elif metric == "canberra":
+                    d = scipy.spatial.distance.canberra(features, queryFeatures)
+                else:
+                    raise NotImplementedError
 
-		#sort in order of relevance
-		results = sorted([(v,k) for (k,v) in results.items()])
+                # Update dictionsary
+                # key is image id, value is similarity
+                results[row[0]] = d
 
-		#return our (limited) results
-		return results[:limit]
+            # close reader
+            f.close()
 
-	def chi2_distance(self, histA, histB, eps=1e-10):
-		#compute chi-squared distance
-		d = 0.5 * np.sum([((a-b)**2)/(a+b+eps)
-			for (a,b) in zip(histA, histB)])
+        # sort in order of relevance
+        results = sorted([(v, k) for (k, v) in results.items()])
 
-		# return the chi-squared distance
-		return d
+        # return our (limited) results
+        return results[:limit]
+
+    def chi2_distance(self, histA, histB, eps=1e-10):
+        # compute chi-squared distance
+        d = 0.5 * np.sum(
+            [((a - b) ** 2) / (a + b + eps) for (a, b) in zip(histA, histB)]
+        )
+
+        # return the chi-squared distance
+        return d
